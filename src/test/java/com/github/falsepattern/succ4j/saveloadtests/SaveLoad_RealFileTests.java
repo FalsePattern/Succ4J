ecc2b2b9ab53e5e1241e20165e1ac18690a7d73b
FalsePattern <thefalsepattern@gmail.com>
2020-04-17 22:58:42 +0200

    Improved maven pom.xml and made all test classes end with "Tests"

--- src/test/java/com/github/falsepattern/succ4j/saveloadtests/SaveLoad_RealFileTests.java
+++ src/test/java/com/github/falsepattern/succ4j/saveloadtests/SaveLoad_RealFileTests.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
 
-public class SaveLoad_RealFile {
+public class SaveLoad_RealFileTests {
     private static final String savedValueKey = "test key";
 
     @Test
